4dbb67483f1ae27e0886bd32500f409d507793ce
yusuf <yusuf@HP_VICTUS-16>
2023-12-09 18:25:55 +0300

    Add project files.

---
+++ ProgramlamaOdev4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramlamaOdev4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int disdongu = 0, icdongu = 0;
+            for (int i = 0; i < 25; i++)
+            {
+                disdongu++;
+                for (int j = 0; j < 5; j++)
+                    icdongu++;
+            }
+            Console.WriteLine("Dış Döngü: " + disdongu);
+            Console.WriteLine("İç Döngü: " + icdongu);
+            Console.ReadLine();
+        }
+    }
+}
